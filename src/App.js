75bf2ac807a5e63f814ab84beb3adf96050f30eb
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-23 11:16:24 -0600

    commit tree f3d7a13337ac095776ebd0e741a13a21544abac6

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+	
+	componentDidMount() {
+		const baseURL = 'http://api.open-notify.org/astros.json'
+		fetch(baseURL)
+			.then(response => response.json())
+			.then(data => console.log(data));
+
+	}
+
+	render() {
+		return (
+			<div></div>
+		)
+	}
+
+}
